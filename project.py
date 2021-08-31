183f200ed4b75ab229d311e2a677d5de97cd5991
Rami-Atbay <rami.gary@hotmail.com>
2021-08-31 21:15:42 +0300

    additional comments made on bikeshare.py file on line 4

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 import time
 import pandas as pd
 import numpy as np
-
+# user input to filter through different cities
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
